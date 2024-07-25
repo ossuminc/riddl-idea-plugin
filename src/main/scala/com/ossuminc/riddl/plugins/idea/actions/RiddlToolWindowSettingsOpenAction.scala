c54d9c3f90d20cf46584b4c24fe3590092a4b3ff
AlWein92 <alex.weinstein@improving.com>
2024-07-25 14:37:13 -0400

    settings page moved to button in ToolWindow [RIP-17]

--- src/main/scala/com/ossuminc/riddl/plugins/idea/actions/RiddlToolWindowSettingsOpenAction.scala
+++ src/main/scala/com/ossuminc/riddl/plugins/idea/actions/RiddlToolWindowSettingsOpenAction.scala
@@ -3,19 +3,17 @@ package com.ossuminc.riddl.plugins.idea.actions
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
 import com.intellij.openapi.project.DumbAware
-import com.ossuminc.riddl.plugins.utils.{getRiddlIdeaState, updateToolWindow}
+import com.ossuminc.riddl.plugins.utils.openToolWindowSettings
 import org.jetbrains.annotations.NotNull
 
-class RiddlToolWindowCompileAction extends AnAction with DumbAware {
+class RiddlToolWindowSettingsOpenAction extends AnAction with DumbAware {
   @Override
   def actionPerformed(@NotNull anActionEvent: AnActionEvent): Unit = {
-    getRiddlIdeaState.getState.clearOutput()
-    updateToolWindow(true)
+    openToolWindowSettings()
   }
-
 
   override def update(e: AnActionEvent): Unit = {
     super.update(e)
-    e.getPresentation.setIcon(AllIcons.Actions.BuildLoadChanges)
+    e.getPresentation.setIcon(AllIcons.Actions.InlayGear)
   }
 }

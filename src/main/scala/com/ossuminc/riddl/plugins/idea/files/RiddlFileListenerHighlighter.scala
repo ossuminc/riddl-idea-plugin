a3be81127496e1d2106aa06a4f5337a9cb946fa5
AlWein92 <alex.weinstein@improving.com>
2024-09-11 14:32:08 -0400

    coloring comments & ounctuation correctly [RIP-36]

--- src/main/scala/com/ossuminc/riddl/plugins/idea/files/RiddlFileListenerHighlighter.scala
+++ src/main/scala/com/ossuminc/riddl/plugins/idea/files/RiddlFileListenerHighlighter.scala
@@ -4,25 +4,23 @@ import com.intellij.ide.highlighter.custom.CustomHighlighterColors
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.intellij.openapi.fileEditor.{FileDocumentManager, TextEditor, FileEditor, FileEditorManager, FileEditorManagerListener, FileEditorProvider, FileEditorWithTextEditors}
+import com.intellij.openapi.fileEditor.{FileDocumentManager, FileEditor, FileEditorManager, FileEditorManagerEvent, FileEditorManagerListener, FileEditorProvider, FileEditorWithTextEditors, TextEditor}
 import com.ossuminc.riddl.plugins.idea.files.RiddlTokenizer.{keywords, punctuation, readability}
-import com.intellij.openapi.editor.Document
-import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.{DefaultLanguageHighlighterColors, Document, Editor}
 import kotlin.Result
 import com.ossuminc.riddl.plugins.utils.ManagerBasedGetterUtils.getProject
-import com.intellij.openapi.editor.markup.{HighlighterTargetArea, HighlighterLayer}
+import com.intellij.openapi.editor.markup.{HighlighterLayer, HighlighterTargetArea}
 import com.intellij.openapi.fileEditor.ex.FileEditorWithProvider
 import com.intellij.util.containers.ContainerUtil
 import com.intellij.openapi.util.Pair
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.editor.colors.{ColorKey, TextAttributesKey}
 
 class RiddlFileListenerHighlighter extends FileEditorManagerListener {
   override def fileOpened(
     source: FileEditorManager,
     file: VirtualFile,
-  ): Unit = {
-    source.getAllEditors(file).foreach { te =>
+  ): Unit = source.getAllEditors(file).foreach { te =>
       val doc = FileDocumentManager.getInstance().getDocument(file)
       if doc != null then {
         te match {
@@ -30,31 +28,54 @@ class RiddlFileListenerHighlighter extends FileEditorManagerListener {
         }
       }
     }
-  }
+
+  //override def selectionChanged(
+  //   event: FileEditorManagerEvent
+  // ): Unit = source.getAllEditors(file).foreach { te =>
+  //  val doc = FileDocumentManager.getInstance().getDocument(file)
+  //  if doc != null then {
+  //    te match {
+  //      case textEditor: TextEditor => highlightKeywords(doc.getText, textEditor.getEditor)
+  //    }
+  //  }
+  //}
 
   private def highlightKeywords(text: String, editor: Editor): Unit = {
     import com.ossuminc.riddl.plugins.idea.files.RiddlTokenizer.*
 
-    RiddlTokenizer.tokenize(text).foreach { (token, index, length) =>
-      if index > -1 && length > 0 then {
-        (if keywords.contains(token) then
-          Some(CUSTOM_KEYWORD_KEYWORD)
-        else if punctuation.contains(token) then
-          Some(CUSTOM_KEYWORD_PUNCTUATION)
-        else if readability.contains(token) then
-          Some(CUSTOM_KEYWORD_READABILITY)
-        else None) match {
-          case Some(colorKey: TextAttributesKey) =>
-            editor.getMarkupModel.addRangeHighlighter(
-              colorKey,
-              index,
-              index + length,
-              HighlighterLayer.SYNTAX,
-              HighlighterTargetArea.EXACT_RANGE
-            )
-          case None => ()
-        }
-      }
+    def applyColourKey(colorKey: TextAttributesKey, index: Int, length: Int, punctIndex: Int, punct: String): Unit = {
+      val (trueIndex: Int, trueLength: Int) =
+        if punctIndex < 0 then (index, length) else (punctIndex + index, punct.length)
+
+      editor.getMarkupModel.addRangeHighlighter(
+        colorKey,
+        trueIndex,
+        trueIndex + trueLength,
+        HighlighterLayer.SYNTAX,
+        HighlighterTargetArea.EXACT_RANGE
+      )
+      ()
+    }
+
+    RiddlTokenizer.tokenize(text).foreach {
+      case (token, index, Seq(isQuoted, isComment)) if isQuoted || isComment =>
+        applyColourKey(
+          if isComment then DefaultLanguageHighlighterColors.LINE_COMMENT
+          else if isQuoted then DefaultLanguageHighlighterColors.STRING
+          else DefaultLanguageHighlighterColors.KEYWORD,
+          index,
+          token.length,
+          -1,
+          ""
+        )
+      case (token, index, _) if index > -1 && token.length > 0 =>
+        if keywords.contains(token) then applyColourKey(CUSTOM_KEYWORD_KEYWORD, index, token.length, -1, "")
+        else if punctuation.exists(token.contains) then
+          punctuation.find(token.contains).foreach(punct =>
+            applyColourKey(CUSTOM_KEYWORD_PUNCTUATION, index, token.length, token.indexOf(punct), punct)
+          )
+        else if readability.contains(token) then applyColourKey(CUSTOM_KEYWORD_READABILITY, index, token.length, -1, "")
+      case (_, _, _) => ()
     }
   }
 }
--- src/main/scala/com/ossuminc/riddl/plugins/idea/files/RiddlTokenizer.scala
+++ src/main/scala/com/ossuminc/riddl/plugins/idea/files/RiddlTokenizer.scala
@@ -6,34 +6,50 @@ import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.psi.CustomHighlighterTokenType
 
 object RiddlTokenizer {
-  def tokenize(text: String): Seq[(String, Int, Int)] = {
+  // Outputs:
+  // String - word
+  // Int - index
+  // Int - length,
+  // Boolean - isComment
+  def tokenize(text: String): Seq[(String, Int, Seq[Boolean])] = {
     var currentIndex = 0
-    tokenizeWithQuotesBoolean(text).map { case (word, inQuotes) =>
-      if !inQuotes then {
-        val tuple = (word, currentIndex, word.length)
-        currentIndex += word.length
-        tuple
-      }
-      else {
-        currentIndex += word.length
-        (word, -1, 0)
-      }
+    tokenizeWithQuotesBoolean(text).map { case (word, Seq(isQuoted, isComment)) =>
+      val tuple = (word, currentIndex, Seq(isQuoted, isComment))
+      currentIndex += word.length
+      tuple
     }
   }
 
-  private def tokenizeWithQuotesBoolean(text: String): Seq[(String, Boolean)] = {
+  private def tokenizeWithQuotesBoolean(text: String): Seq[(String, Seq[Boolean])] = {
     var inQuotes: Boolean = false
-    text.split("((?<=\\s+)|(?=\\s+))").toSeq.foldLeft(List[(String, Boolean)]()) {
-      case (acc: List[(String, Boolean)], word: String) =>
-        if word.endsWith("\"") && word.startsWith("\"") then
-          inQuotes = false
-          acc :+ (word, false)
-        else if word.contains("\"") then {
-          val tokBool = acc :+ (word, !inQuotes)
-          inQuotes = !inQuotes
-          tokBool
-        }
-        else acc :+ (word, inQuotes)
+    var inComment: Boolean = false
+
+    def checkForComment(word: String): Unit =
+      if word.startsWith("//") && !inComment then
+        inComment = true
+      else if word == "\n" && inComment then
+        inComment = false
+
+    def checkForQuotes(word: String): Boolean =
+      if word.count(c => c == '\"') == 2 then
+        println("two")
+        true
+      else if word.startsWith("\"") then
+        println("start")
+        inQuotes = true
+        inQuotes
+      else if word.endsWith("\"") then
+        println("end")
+        inQuotes = false
+        true
+      else
+        println("none")
+        inQuotes
+
+    text.split("((?<=\\s+)|(?=\\s+))").toSeq.foldLeft(List[(String, Seq[Boolean])]()) {
+      case (acc: List[(String, Seq[Boolean])], word: String) =>
+        checkForComment(word)
+        acc :+ (word, Seq(checkForQuotes(word), inComment))
     }
   }
 
@@ -43,18 +59,17 @@ object RiddlTokenizer {
     "direct", "presents", "do", "domain", "else", "email", "end", "entity", "epic", "error", "event", "example",
     "execute", "explained", "field", "fields", "file", "flow", "focus", "foreach", "form",
     "function", "graph", "group", "handler", "if", "import", "include", "index", "init", "inlet", "inlets",
-    "input", "invariant", "items", "many", "mapping", "merge", "message", "morph", "name", "one", "organization",
+    "input", "invariant", "items", "many", "mapping", "merge", "message", "morph", "name", "on", "one", "organization",
     "option", "optional", "options", "other", "outlet", "outlets", "output", "parallel", "pipe", "plant", "projector",
     "query", "range", "reference", "remove", "replica", "reply", "repository", "requires", "required", "record",
     "result", "results", "return", "returns", "reverted", "router", "saga", "schema", "selects", "send", "sequence",
     "set", "show", "shown", "sink", "source", "split", "state", "step", "stop", "story", "streamlet", "table", "take",
     "tell", "term", "then", "title", "type", "url", "updates", "user", "value", "void", "when", "where")
 
-  val punctuation: Seq[String] = Seq("asterisk", "comma", "colon", "curlyOpen", "curlyClose", "dot", "equalsSign",
-    "ellipsis", "ellipsisQuestion", "exclamation", "plus", "question", "quote", "roundOpen", "roundClose",
-    "squareOpen", "squareClose", "undefinedMark", "verticalBar", "{", "}", "(", ")")
+  val punctuation: Seq[String] = Seq("@", "*", ":", ".", "=", "...", "!", "+", "?", "[",
+    "]", "???", "|", "{", "}", "(", ")")
 
-  val readability: Seq[String] = Seq("and", "are", "as", "at", "by", "for", "from", "in", "is", "of", "on",
+  val readability: Seq[String] = Seq("and", "are", "as", "at", "by", "for", "from", "in", "is", "of",
     "so", "that", "to", "wants", "with")
 
   val CUSTOM_KEYWORD_KEYWORD: TextAttributesKey = CustomHighlighterColors.CUSTOM_KEYWORD2_ATTRIBUTES

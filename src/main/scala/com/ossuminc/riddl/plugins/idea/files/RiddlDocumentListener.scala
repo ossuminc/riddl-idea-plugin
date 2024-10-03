e34be31696206525c9d8a8af76d4e9ffe77c2c17
AlWein92 <alex.weinstein@improving.com>
2024-10-03 12:05:13 -0400

    no riddlc jar [RIP-59]

--- src/main/scala/com/ossuminc/riddl/plugins/idea/files/RiddlDocumentListener.scala
+++ src/main/scala/com/ossuminc/riddl/plugins/idea/files/RiddlDocumentListener.scala
@@ -12,27 +12,25 @@ import com.ossuminc.riddl.plugins.idea.files.RiddlTokenizer.*
 class RiddlDocumentListener extends DocumentListener {
   override def documentChanged(event: DocumentEvent): Unit = {
     val doc = event.getDocument
-    if doc != null then {
-      val editors = EditorFactory.getInstance().getEditors(doc)
-      if editors.nonEmpty then
-        val newText = doc.getText(
-          new TextRange(event.getOffset, event.getOffset + event.getNewLength)
-        )
-        val wholeWords = getWholeWordsSubstrings(
-          doc.getText,
-          newText,
-          event.getOffset
-        )
-        highlightKeywordsInDoc(
-          wholeWords
-            .zip(
-              RiddlTokenizer
-                .tokenize(wholeWords.map(_._1).mkString(" "))
-                .filter(!_._1.isBlank)
-            )
-            .map((wwTup, tokTup) => (wwTup._1, wwTup._2, tokTup._3)),
-          editors.head
-        )
-    }
+    val editors = EditorFactory.getInstance().getEditors(doc)
+    if editors.nonEmpty then
+      val newText = doc.getText(
+        new TextRange(event.getOffset, event.getOffset + event.getNewLength)
+      )
+      val wholeWords = getWholeWordsSubstrings(
+        doc.getText,
+        newText,
+        event.getOffset
+      )
+      highlightKeywordsInDoc(
+        wholeWords
+          .zip(
+            RiddlTokenizer
+              .tokenize(wholeWords.map(_._1).mkString(" "))
+              .filter(!_._1.isBlank)
+          )
+          .map((wwTup, tokTup) => (wwTup._1, wwTup._2, tokTup._3)),
+        editors.head
+      )
   }
 }
